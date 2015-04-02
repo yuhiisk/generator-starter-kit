725a1ba574171b50ef6da108c549cbbe5b61e677
yuhiisk <youheiisokawa@gmail.com>
2015-04-03 00:07:06 +0900

    init generator

--- app/index.js
+++ app/index.js
@@ -1,22 +1,62 @@
-"use strict";
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
 
-var generators = require('yeoman-generator');
+module.exports = yeoman.generators.Base.extend({
+  initializing: function () {
+    this.pkg = require('../package.json');
+  },
 
-module.exports = generators.Base.extend({
-    // The name `constructor` is important here
-    constructor: function() {
-        // Calling the super constructor
-        generators.Base.apply(this, arguments);
+  prompting: function () {
+    var done = this.async();
 
-        // Next
-        this.option('coffee');
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the rad ' + chalk.red('StarterKit') + ' generator!'
+    ));
+
+    var prompts = [{
+      type: 'confirm',
+      name: 'someOption',
+      message: 'Would you like to enable this option?',
+      default: true
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.someOption = props.someOption;
+
+      done();
+    }.bind(this));
+  },
+
+  writing: {
+    app: function () {
+      this.fs.copy(
+        this.templatePath('_package.json'),
+        this.destinationPath('package.json')
+      );
+      this.fs.copy(
+        this.templatePath('_bower.json'),
+        this.destinationPath('bower.json')
+      );
     },
 
-    method1: function() {
-        console.log('method 1 just run');
-    },
-
-    method2: function() {
-        console.log('method 2 just run');
+    projectfiles: function () {
+      this.fs.copy(
+        this.templatePath('editorconfig'),
+        this.destinationPath('.editorconfig')
+      );
+      this.fs.copy(
+        this.templatePath('jshintrc'),
+        this.destinationPath('.jshintrc')
+      );
     }
+  },
+
+  install: function () {
+    this.installDependencies({
+      skipInstall: this.options['skip-install']
+    });
+  }
 });
